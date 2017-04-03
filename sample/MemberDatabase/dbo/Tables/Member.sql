880b537431a843896d04fbc60f18c3ff2cd3fa76
Rahul <joshirahul21@gmail.com>
2017-04-03 13:09:58 -0400

    Added SQL Project to hold all the queries related to the project.

---
+++ sample/MemberDatabase/dbo/Tables/Member.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Member] (
+    [Id]          BIGINT        IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (50) NOT NULL,
+    [LastName]    NVARCHAR (50) NULL,
+    [DateOfBirth] DATETIME      NOT NULL,
+    [Sex]         CHAR (1)      NOT NULL,
+    [MotherId]    BIGINT        NULL,
+    [FatherId]    BIGINT        NULL,
+    CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (PAD_INDEX = ON),
+    CONSTRAINT [FK_Father_Member] FOREIGN KEY ([FatherId]) REFERENCES [dbo].[Member] ([Id]),
+    CONSTRAINT [FK_Mother_Member] FOREIGN KEY ([MotherId]) REFERENCES [dbo].[Member] ([Id])
+);
+
